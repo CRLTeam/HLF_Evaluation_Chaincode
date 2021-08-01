c454ecda96abc62637394f36f3219bfa6433a5ae
Dave McKay <dave@promulgareconsulting.com>
2021-08-01 14:07:09 -0400

    Create evaluation.js
    Initial commit

---
+++ evaluation.js
@@ -0,0 +1,49 @@
+/*
+ * Ryserson Cybersecurity Research Lab
+ * Dave McKay
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class Evaluation extends Contract {
+
+    async InitLedger(ctx) {
+        await ctx.stub.putState('StateData', Buffer.from('0'));
+        console.info('Evaluation stateData initialized');
+    }
+
+    // Write the stateData value.
+    async WriteState(ctx, _stateData) {
+        ctx.stub.putState('StateData', Buffer.from(_stateData));
+        await this.ctx.stub.setEvent('watchState', Buffer.from(JSON.stringify(stateData)));
+        return _stateData;
+    }
+
+    // Read the stateData value.
+    async ReadState(ctx) {
+        const _stateData = await ctx.stub.getState('StateData'); 
+        return _stateData;
+    }
+
+    // Call the interop function
+    async callInterop(ctx, _interopDID, _func, _value, _callerDID, _nonce, _sig) {
+        let callData = {
+            interopDID: _interopDID,
+            func: _func,
+            value: _value,
+            callerDID, _callerDID,
+            nonce: _nonce,
+            sig: _sid,                              // encrypted hash of previous values
+            txID: ctx.stub.getTxID(),               // transaction proposal ID
+            userID: ctx.clientIdentity.getID(),     // user ID
+            orgID: ctx.clientIdentity.getMSPID()    // org information
+        }
+        await this.ctx.stub.setEvent('interopCall', Buffer.from(JSON.stringify(callData)));
+    }
+}
+
+module.exports = Evaluation;
